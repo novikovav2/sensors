87995e2b3105de4d596efc67b65e85e24a02e8d1
Aleksandr Novikov <novikovav2@gmail.com>
2021-01-20 17:39:11 +0400

    Add first view of charts

--- app/controllers/startic_pages_controller.rb
+++ app/controllers/startic_pages_controller.rb
@@ -2,6 +2,27 @@ class StarticPagesController < ApplicationController
 
   # ROOT page
   def index
+    # @house_last = Temperature.find_by_sensor('house').last
+    # @bath_last = Temperature.find_by_sensor('banya').last
+    @house_data = []
+    @house_data = Temperature.where(sensor: 'house').pluck(:created_at, :data)
+    # house_data_all = Temperature.where(sensor: 'house')
+    # if house_data_all
+    #   house_data_all.each do |temp|
+    #     data = {time: temp.created_at, value: temp.data}
+    #     @house_data << data
+    #   end
+    # end
+    puts @house_data
+
+    @bath_data = []
+    bath_data_all = Temperature.where(sensor: 'banya')
+    if bath_data_all
+      bath_data_all.each do |temp|
+        data = {time: temp.created_at, value: temp.data}
+        @bath_data << data
+      end
+    end
 
   end
 end
