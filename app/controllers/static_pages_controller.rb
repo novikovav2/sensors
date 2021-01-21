85cf8ed45f50dae4d8d8bd7585c822f71ae39c8a
Aleksandr Novikov <novikovav2@gmail.com>
2021-01-21 12:05:49 +0400

    Add some css

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,12 +4,14 @@ class StaticPagesController < ApplicationController
   def index
     start_time_interval = 12.hours.ago
     @house_data = Temperature.where('sensor = "house" and created_at > ?', start_time_interval)
+                              .order(:created_at)
                              .pluck(:created_at, :data)
     @bath_data = Temperature.where('sensor = "banya" and created_at > ?', start_time_interval)
+                            .order(:created_at)
                             .pluck(:created_at, :data)
 
-    @data = [{name: 'Дом', data: @house_data, color: 'red'},
-             { name: 'Баня', data: @bath_data, color: 'green' }
+    @data = [{name: 'Дом', data: @house_data, color: '#e79592'},
+             { name: 'Баня', data: @bath_data, color: '#6ab8f7' }
     ]
 
     @last_house_data = Temperature.where(sensor: 'house').order(:created_at).last
